4868f499bec003ab635d71416e564b41d029c871
Unknown <oliver.matonoha@centrum.cz>
2018-11-15 13:31:13 +0100

    i

--- readTree_V0.C
+++ readTree_V0.C
@@ -11,6 +11,8 @@ using namespace std;
 
 TChain* mChain;
 AliAnalysisPIDEvent* mEvent;
+TClonesArray* bTracks;
+TClonesArray* bV0s;
 
 bool makeChain(const Char_t *inputFile="test.list") {
 
@@ -51,8 +53,9 @@ bool makeChain(const Char_t *inputFile="test.list") {
 
 bool SelectEvent(AliAnalysisPIDEvent* e) {
 
-	cout << "ae is " << e->AcceptEvent(1,1) << endl;
-	if (!e->AcceptEvent(1,1)) return false;
+	//cout << "ae is " << e->AcceptEvent(1,1) << endl;
+	if (!e->AcceptEvent(1,0)) 			return false;
+	if (bV0s->GetEntriesFast() < 1) 	return false;
 	return true; 
 
 }
@@ -65,24 +68,28 @@ void readTree_V0(Int_t nEvents=10, const Char_t *inputFile="test.list", const Ch
 	if (!makeChain(inputFile)) printf("Couldn't create the chain! \n", );
 	else printf("Chain created with %i entries \n", mChain->GetEntries());
 
-	TClonesArray *bTracks = new TClonesArray("AliAnalysisPIDTrack");
+	*bTracks 	= new TClonesArray("AliAnalysisPIDTrack");
+	*bV0s 		= new TClonesArray("AliAnalysisPIDV0");
 	mChain->SetBranchAddress("AnalysisTrack",&bTracks);
+	mChain->SetBranchAddress("AnalysisV0Track",&bV0s);
 	mChain->SetBranchAddress("AnalysisEvent",&mEvent);
+
+	TH1F* hEventMonitor			= new TH1F("hEventMonitor","",10,-0.5,9.5);
 
 	nEvents = (nEvents < mChain->GetEntries()) ? nEvents : mChain->GetEntries();
 
 	for (int iEv = 0; iEv < nEvents; ++iEv)	{
 		
+		hEventMonitor->Fill(0);
 		bTracks->Clear();
 		mChain->GetEntry(iEv);
 		//printf("event vz is %f \n", mEvent->GetVertexZ());
 
 		if (iEv==0) mEvent->PrintEventSelection();
+		//precut histos
 		if (!SelectEvent(mEvent)) continue;
-
-		//precut event histos
-		//select event
-		//aftercut event histos
+		//aftercut histos
+		hEventMonitor->Fill(1);
 
 		Int_t nTracks = bTracks->GetEntriesFast();
 		printf("nTracks is %i \n", nTracks);
@@ -101,4 +108,5 @@ void readTree_V0(Int_t nEvents=10, const Char_t *inputFile="test.list", const Ch
 	}
 
 	printf(" WHAT IS UP \n", );
+	hEventMonitor->Draw();
 }
