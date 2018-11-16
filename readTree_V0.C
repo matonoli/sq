b2be734575bb98016e0b2956643febf53424926e
Unknown <oliver.matonoha@centrum.cz>
2018-11-16 13:57:58 +0100

    i

--- readTree_V0.C
+++ readTree_V0.C
@@ -150,19 +150,36 @@ void readTree_V0(Int_t nEvents=10, const Char_t *inputFile="test.list", const Ch
 	mChain->SetBranchAddress("AnalysisV0Track",&bV0s);
 	mChain->SetBranchAddress("AnalysisEvent",&mEvent);
 
+	const Int_t nPtBins = 59;
+  	Double_t xBins[nPtBins+1] = { 0.01, 0.10, 0.12, 0.14, 0.16, 0.18, 0.20, 0.25, 0.30, 0.35, 
+    	0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 
+    	0.90, 0.95, 1.00, 1.10, 1.20, 1.30, 1.40, 1.50, 1.60, 1.70, 
+    	1.80, 1.90, 2.00, 2.20, 2.40, 2.60, 2.80, 3.00, 3.20, 3.40, 
+    	3.60, 3.80, 4.00, 4.50, 5.00, 5.50, 6.00, 6.50, 7.00, 8.00, 
+    	9.00, 10.00, 11.00, 12.00, 13.00, 14.00, 15.00, 16.00, 18.00, 20.00 };
+
+
 	TH1F* hEventMonitor			= new TH1F("hEventMonitor","",10,-0.5,9.5);
 	TH1F* hV0Monitor			= new TH1F("hV0Monitor","",10,-0.5,9.5);
 	TH1F* hTrackMonitor			= new TH1F("hTrackMonitor","",10,-0.5,9.5);
 	TH2F* hV0TrCounter			= new TH2F("hV0TrCounter","",100,0,100,100,0,100);
 
-	TH1F* hV0_IMK0s				= new TH1F("hV0_IMK0s","",2000,-1,1);
-	TH1F* hV0_IML				= new TH1F("hV0_IML","",2000,-1,1);
-	TH1F* hV0_IMAL				= new TH1F("hV0_IMAL","",2000,-1,1);
+	TH1F* hV0_IMK0s				= new TH1F("hV0_IMK0s","",2000,-0.2,0.2);
+	TH1F* hV0_PtK0s				= new TH1F("hV0_PtK0s","",nPtBins,xBins);
+	TH1F* hV0_IML				= new TH1F("hV0_IML","",2000,-0.2,0.2);
+	TH1F* hV0_PtL				= new TH1F("hV0_PtL","",nPtBins,xBins);
+	TH1F* hV0_IMAL				= new TH1F("hV0_IMAL","",2000,-0.2,0.2);
+	TH1F* hV0_PtAL				= new TH1F("hV0_PtAL","",nPtBins,xBins);
+	hV0_IMK0s->Sumw2();
+	hV0_PtK0s->Sumw2();
+	hV0_IML->Sumw2();
+	hV0_PtL->Sumw2();
+	hV0_IMAL->Sumw2();
+	hV0_PtAL->Sumw2();
 
 	TH2F* hV0_DDTofPiPi			= new TH2F("hV0_DDTofPiPi","",300,-15,15,300,-15,15);
 	TH2F* hV0_DDTofPiP			= new TH2F("hV0_DDTofPiP","",300,-15,15,300,-15,15);
 	TH2F* hV0_DTofPivp			= new TH2F("hV0_DTofPivp","",100,0,10,300,-15,15);
-
 	TH2F* hV0_DDDedx			= new TH2F("hV0_DDDedx","",300,0,300,300,0,300);
 	TH2F* hV0_DDedxvp			= new TH2F("hV0_DDedxvp","",100,0,10,300,0,300);
 
@@ -205,11 +222,14 @@ void readTree_V0(Int_t nEvents=10, const Char_t *inputFile="test.list", const Ch
 
 			bool noCuts = 0;
 			if (noCuts || IsK0s(v0)) 	{
-				hV0_IMK0s->Fill(v0->GetIMK0s());	}
+				hV0_IMK0s->Fill(v0->GetIMK0s());	
+				hV0_PtK0s->Fill(v0->GetPt()) 		}
 			if (noCuts || IsL(v0)) 		{
-				hV0_IML->Fill(v0->GetIML());		}
+				hV0_IML->Fill(v0->GetIML());		
+				hV0_PtL->Fill(v0->GetPt());			}
 			if (noCuts || IsAL(v0)) 	{
-				hV0_IMAL->Fill(v0->GetIMAL());		}
+				hV0_IMAL->Fill(v0->GetIMAL());
+				hV0_PtAL->Fill(v0->GetPt());		}
 		}
 
 		Int_t trCount = 0;
