6b021e4d9d9728b81af249d776d3fd51b0147c9b
Unknown <oliver.matonoha@centrum.cz>
2018-11-19 16:14:31 +0100

    i

--- readTree_V0.C
+++ readTree_V0.C
@@ -280,7 +280,7 @@ void readTree_V0(Int_t nEvents=10, Int_t cutFlag=0, const Char_t *inputFile="tes
 
 	for (int iBin = 0; iBin < nPtBins; ++iBin)
 	{
-		hYieldK0s->SetBinContent(iBin+1,ExtractYield(hV0_IMPtK0s->ProjectionY("y",iBin,iBin+1)));
+		hYieldK0s->SetBinContent(iBin+1,ExtractYield(hV0_IMPtK0s->ProjectionX("x",iBin,iBin)));
 	}
 
 	hV0_PtK0s->Scale(1,"width");
