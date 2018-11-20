af7117ff19ed999d820bbba3f37c9af810269718
Unknown <oliver.matonoha@centrum.cz>
2018-11-20 14:34:33 +0100

    f

--- readTree_V0.C
+++ readTree_V0.C
@@ -361,9 +361,11 @@ void readTree_V0(Int_t nEvents=10, Int_t cutFlag=0, const Char_t *inputFile="tes
 	for (int iBin = 0; iBin < 9; ++iBin)
 	{
 		//if (iBin!= 279) continue;
-		hYieldK0s->SetBinContent(iBin,*(ExtractYield(hV0_IMPtK0s->ProjectionX("x",iBin,iBin))+0));
-		hYieldL->SetBinContent(iBin,*(ExtractYield(hV0_IMPtL->ProjectionX("x",iBin,iBin))+0));
-		hYieldAL->SetBinContent(iBin,*(ExtractYield(hV0_IMPtAL->ProjectionX("x",iBin,iBin))+0));
+		Float_t *p = ExtractYield(hV0_IMPtK0s->ProjectionX("x",iBin,iBin));
+		hYieldK0s->SetBinContent(iBin,*(p+0));
+		//hYieldK0s->SetBinContent(iBin,*(ExtractYield(hV0_IMPtK0s->ProjectionX("x",iBin,iBin))+0));
+		//hYieldL->SetBinContent(iBin,*(ExtractYield(hV0_IMPtL->ProjectionX("x",iBin,iBin))+0));
+		//hYieldAL->SetBinContent(iBin,*(ExtractYield(hV0_IMPtAL->ProjectionX("x",iBin,iBin))+0));
 	}
 
 	hV0_PtK0s->Scale(1,"width");
